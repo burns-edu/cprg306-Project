8733718008faa1303ec56fc512f5cb641b398a65
Georg Chervyakov <Georgech8765@gmail.com>
2026-03-31 14:53:45 -0600

    your message here

---
+++ app/cart/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React from 'react'
+import '../globals.css'
+
+export default function cart() {
+
+
+
+  return (
+
+    <div className='container-cart'>
+
+
+      <header>
+        <h1 className='cart-title-text'>Shopping Cart</h1>
+      </header>
+
+      <main>
+          <p className='checkout-title-text'>Checkout</p>
+      </main>
+
+
+
+    
+
+
+
+
+
+    </div>
+  )
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,45 @@
-import Image from "next/image";
+'use client';
+
+
+import React from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import './globals.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import router from 'next/dist/shared/lib/router/router';
+
 
 export default function Home() {
+
+  const router = useRouter();
+
+
+
+
+
+
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-gray-50 font-sans dark:bg-black">
+
+
+
+      <button onClick={() => router.push('../cart')}>
+        <FontAwesomeIcon icon={faShoppingCart} className='cart' />
+      </button>
+
 
       <header className="header">
         <h1
           className="
           text-[50px]
           text-black
-          dark: text-white
+          dark:text-white
           ">Welcome to Haylo</h1>
+
+
+
       </header>
 
       <main className="main">
